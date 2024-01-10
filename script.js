7789705b0f656c684576724df72585cd4cfae4d3
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2024-01-10 10:42:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Create an object called student with a property called name
+var student = {
+  name: "John"
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+  // Return an array of all the keys in the object
+  return Object.keys(this);
+};
+
+// Use the getKeys() method on the student object
+var keysArray = student.getKeys();
+
+// Display the result
+console.log(keysArray);
